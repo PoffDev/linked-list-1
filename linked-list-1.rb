490f26263421401a410568e56a9f27a7918caf34
PoffDev <joshuapoffenberger@gmail.com>
2019-06-12 12:44:51 -0400

    updated changes

--- linked-list-1.rb
+++ linked-list-1.rb
@@ -7,6 +7,15 @@ class LinkedListNode
   end
 end  # end of LinkedListNode
 
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+  end
+end
+
 class Stack
   attr_reader :linked_list_node
 
@@ -15,55 +24,21 @@ class Stack
   end
 
   def push(value)
-    @linked_list_node = LinkedListNode.new(@value, @linked_list_node)
+    @linked_list_node = LinkedListNode.new(value, @linked_list_node)
   end
 
-  def pop
-    popped_value = nil
-    
-    if @linked_list_node
-      popped_value = @linked_list_node.value
-      @linked_list_node = @linked_list_node.next_node
-    end
-
-    return popped_value
-  end
 end # end of Stack
-
-# Algorithm:
-#create a new stack
-#add the nodes in order to the stack (push)
-#pop the nodes off the stack...
-#add node to reversed_list
-#LinkedListNode.new(node.value, node.next_node)
 
 def reverse_list(list)
   stack = Stack.new()
 
   #add the nodes in order to the stack (push)
   while list
-    stack.push(list)
+    stack.push(list.value)
     list = list.next_node
   end
 
-  node = stack.pop()
-
-  #add nodes to reversed_list
-  while node
-    reversed_list = LinkedListNode.new(@next_node.value, node.next_node)
-    node = stack.pop()
-  end 
-
-  return reversed_list
-end
-
-def print_values(list_node)
-  if list_node
-    print "#{list_node.value} --> "
-    print_values(list_node.next_node)
-  else
-    print "nil\n"
-  end
+  stack.linked_list_node
 end
 
 node = LinkedListNode.new(37)
