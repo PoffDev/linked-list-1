37ae603054366ca5964af6768da91117b0256aca
SiriMitchell <48263053+SiriMitchell@users.noreply.github.com>
2019-06-12 11:09:56 -0500

    Add files via upload

--- linked-list-1.rb
+++ linked-list-1.rb
@@ -1,80 +1,79 @@
 class LinkedListNode
   attr_accessor :value, :next_node
 
-
   def initialize(value, next_node=nil)
     @value = value
     @next_node = next_node 
-
   end
-end
-
-
+end  # end of LinkedListNode
 
 class Stack
   attr_reader :linked_list_node
 
   def initialize
-    @linked_list_node = nil
+    @linked_list_node = nil #linked_list_node is the stack
   end
 
   def push(value)
-    linked_list_node = LinkedListNode.new(value, linked_list_node)
+    @linked_list_node = LinkedListNode.new(@value, @linked_list_node)
   end
 
+  def pop
+    popped_value = nil
+    
+    if @linked_list_node
+      popped_value = @linked_list_node.value
+      @linked_list_node = @linked_list_node.next_node
+    end
 
-  def pop 
-    if linked_list_node
-      return linked_list_node.next_node
-    else
-      return nil  
+    return popped_value
   end
-end
+end # end of Stack
 
-
+# Algorithm:
 #create a new stack
 #add the nodes in order to the stack (push)
 #pop the nodes off the stack...
 #add node to reversed_list
 #LinkedListNode.new(node.value, node.next_node)
 
-
 def reverse_list(list)
- stack = Stack.new
+  stack = Stack.new()
 
-    while list 
-      stack.push(list)
-      list = list.next_node
-    end
+  #add the nodes in order to the stack (push)
+  while list
+    stack.push(list)
+    list = list.next_node
+  end
 
-    node = stack.pop
+  node = stack.pop()
 
-    while node
-      reversed_list = LinkedListNode.new(node.value, node.next_node)
-      node = stack.pop
-    end 
+  #add nodes to reversed_list
+  while node
+    reversed_list = LinkedListNode.new(@next_node.value, node.next_node)
+    node = stack.pop()
+  end 
 
-    return reversed_list
-  
+  return reversed_list
 end
 
-
-  def print_values(list_node)
-    if list_node
-      print "#{list_node.value} --> "
-      print_values(list_node.next_node)
-    else
-      print "nil\n"
-    end
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
   end
+end
 
 node = LinkedListNode.new(37)
 node = LinkedListNode.new(99, node)
 node = LinkedListNode.new(12, node)
 
+ puts "printing the original list"
+ print_values(node)
+ puts " "
 
-  print_values(revlist)
-
-  puts " "
-
-  revlist = reverse_list(revlist)
+ revlist = reverse_list(node)
+ puts "printing the reversed list"
+ print_values(revlist)
